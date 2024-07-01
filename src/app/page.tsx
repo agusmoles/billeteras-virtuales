cb0f6cfa5387f5fd256f4e78b3fee7f4bc57bb2b
Agustin Moles <agusmoles@gmail.com>
2024-07-01 14:30:15 -0300

    Updates companies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
         </div>
 
         <div>
-          Actualizado al 24 de Junio de 2024
+          Actualizado al 01 de Julio de 2024
           <br />
           Creado por{" "}
           <a
