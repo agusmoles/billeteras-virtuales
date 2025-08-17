f79d44b471feaa3a3b24b73908a6a628cec0dd88
agusmoles <agusmoles@gmail.com>
2025-08-17 20:07:17 -0300

    feat: updates companies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
         </div>
 
         <div>
-          Actualizado al 27 de Julio de 2025
+          Actualizado al 17 de Agosto de 2025
           <br />
           Creado por{" "}
           <a
